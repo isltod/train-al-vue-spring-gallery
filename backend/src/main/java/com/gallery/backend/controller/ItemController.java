b184483f58042153dcfa6afbc543915ef40dedfd
E. K. Lee <wolf@teoal.net>
2026-01-01 20:25:44 +0900

    마리아디비까지 설치 연결

---
+++ backend/src/main/java/com/gallery/backend/controller/ItemController.java
@@ -0,0 +1,26 @@
+package com.gallery.backend.controller;
+
+import com.gallery.backend.entity.Item;
+import com.gallery.backend.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    private ItemRepository itemRepository;
+    @Autowired
+    public void setItemRepository(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping("/api/items")
+    public List<Item> getItems() {
+        List<Item> items = itemRepository.findAll();
+        return items;
+    }
+}
