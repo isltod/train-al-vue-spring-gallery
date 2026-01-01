1e0b5ee3761ed36421f88db3fd7e4012884ba8c6
E. K. Lee <wolf@teoal.net>
2026-01-01 16:55:59 +0900

    부트스트랩에서 프론트엔드 골격 잡기

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # train-al-vue-spring-gallery
+유튜버 아프리카도서관 스프링부트 REST API를 이용하여 쇼핑몰 만들기(JPA, MariaDB, JWT) 실습 
---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Home from "@/pages/Home.vue";
+</script>
+
+<template>
+  <Header/>
+  <Home/>
+  <Footer/>
+</template>
+
+<style scoped>
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+</style>
